9c325e07493f12b4653d28f5bb60886edef98960
Ananya Bhat <anyabhat18@gmail.com>
2024-02-26 10:20:39 +0000

    commit_2

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    googleClientID:'946814986538-rg2haqe1aa9l5oai9b770qobakasam65.apps.googleusercontent.com',
+    googleClientSecret: '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'
+};
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+require('./services/passport');
 
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there'});
-});
-
+//returns a function with a callback function 
+require('./routes/authRoutes')(app); 
 
 const PORT=process.env.PORT ||5000; 
 app.listen(PORT);
+
